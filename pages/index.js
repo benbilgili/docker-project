cc738a2ca059dc9eab456d4b5678cc4d447d211d
Ben Bilgili - 2i <ben.bilgili@2itesting.com>
2023-11-17 09:30:00 +0000

    displaying team info

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 function Home() {
   const [data, setData] = useState(null);
+  const [teamId, setTeamId] = useState(57); // Initial teamId
 
   const fetchData = async () => {
     try {
-      const response = await fetch('http://localhost:5000/getFootballData'); // Update the endpoint accordingly
+      const response = await fetch(`http://localhost:5000/getFootballData/${teamId}`);
       const newData = await response.json();
       setData(newData);
       console.log("NEW DATA: ", newData);
@@ -16,19 +17,69 @@ function Home() {
 
   useEffect(() => {
     fetchData();
-  }, []); // Empty dependency array ensures the effect runs only once when the component mounts
+  }, [teamId]); // Run the effect whenever teamId changes
+
+  const handleTeamIdChange = (newTeamId) => {
+    setTeamId(newTeamId);
+  };
 
   return (
     <div>
-      <button onClick={fetchData}>Fetch Data</button>
       <div>
-        <h1>HELLO ARSENAL</h1>
-        {data && (
+        <label>
+          Select Team:
+          <select value={teamId} onChange={(e) => handleTeamIdChange(e.target.value)}>
+
+            <option key={57} value={57} label={"Arsenal"}></option>
+            <option key={58} value={58} label={"Aston Villa"}></option>
+            <option key={61} value={61} label={"Chelsea"}></option>
+            <option key={62} value={62} label={"Everton"}></option>
+            <option key={63} value={63} label={"Fulham"}></option>
+            <option key={64} value={64} label={"Liverpool"}></option>
+            <option key={65} value={65} label={"Manchester City"}></option>
+            <option key={66} value={66} label={"Manchested United"}></option>
+            <option key={67} value={67} label={"Newcastle United"}></option>
+            <option key={73} value={73} label={"Tottenham Hotspur"}></option>
+            <option key={76} value={76} label={"Wolverhampton Wanderers"}></option>          
+
+          </select>
+        </label>
+
+
+
+
+
           <div>
-            <h2>{data.name}</h2>
-            {/* Render other properties as needed */}
-          </div>
-        )}
+
+      {data && (
+        <div>
+          <h2>{data.name}</h2>
+          <h3>Players</h3>
+          {data.squad.map((player) => (
+            <div
+              key={player.id}
+              style={{
+                padding: "0.5rem",
+                border: "1px solid black",
+                borderRadius: "15px",
+                marginTop: "5px",
+                marginBottom: "5px",
+              }}
+            >
+              <span style={{ display: "block", width: "100%" }}>
+                {player.name}
+              </span>
+              <span style={{ display: "block", width: "100%" }}>
+                {player.position}
+              </span>
+            </div>
+          ))}
+          {/* Render other properties as needed */}
+        </div>
+      )}
+    </div>
+
+
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -2,27 +2,31 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 5000;
-
 const apiKey = '71e212815fa947ae8df34bcc4c4cddc0';
 
-app.get('/get-football-data', async (req, res) => {
-  try {
-    const response = await axios.get('https://api.football-data.org/v4/teams/57', {
-      headers: {
-        'X-Auth-Token': apiKey,
-      },
-    });
-
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
-// Enable CORS for all routesapp.use(cors());
-// routesapp.use(cors());
+app.get('/getFootballData/:teamId', async (req, res) => {
+    const { teamId } = req.params;
+  
+    try {
+      const response = await axios.get(`https://api.football-data.org/v4/teams/${teamId}`, {
+        headers: {
+          'X-Auth-Token': apiKey,
+        },
+      });
+  
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
